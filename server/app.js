7ee1d343766c3f71ccbdc769a34f67b394bf00a5
Natsha Goh <gohnatasha98@gmail.com>
2021-06-22 17:11:24 -0400

    initial riot api calls

--- server/app.js
+++ server/app.js
@@ -1,10 +1,50 @@
 const express = require('express')
+const axios = require('axios');
+
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/StormRazr?api_key=${process.env.RIOT_API_KEY}`)
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .then(() => {
+      res.send('Hello World!')
+    });   
+})
+
+app.get('/:summoner', (req, res) => {
+  let param = req.params.summoner;
+  let summoner_data = {}
+  axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${param}?api_key=${process.env.RIOT_API_KEY}`)
+    .then(res => {
+      summoner_data = res.data;
+      let encryptedSummonerId = summoner_data.id;
+      axios.get(`https://na1.api.riotgames.com//lol/league/v4/entries/by-summoner/${encryptedSummonerId}?api_key=${process.env.RIOT_API_KEY}`)
+        .then(res => {
+          console.log(res.data)
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .then(() => {
+      res.send('summoner path')
+    });  
+
+  
+
+})
+
 app.listen(
   PORT, 
   () => console.log(`app listening at http://localhost:${PORT} ${process.env.RIOT_API_KEY}`)
