5856387e69888ac2dedc38db37faf4f5b6c25edf
Natsha Goh <gohnatasha98@gmail.com>
2021-06-28 18:16:32 -0400

    home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import './Home.css'
+
 export default function Home() {
-    return (
-        <div>
-            hi
-        </div>
-    )
+	return (
+		<div className="home-container">
+			<div>
+				<p className="home-title">
+					League Stats
+				</p>
+				<input 
+					className="home-input"
+					type="text"
+					value="summoner name"/>
+			</div>
+		</div>
+	)
 }
