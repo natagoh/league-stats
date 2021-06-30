c94d2d8ea109af336d9c625858e836f9dbf73dd4
Natsha Goh <gohnatasha98@gmail.com>
2021-06-30 18:47:47 -0400

    profile and ranked api

--- server/app.js
+++ server/app.js
@@ -23,28 +23,57 @@ app.get('/', (req, res) => {
     });   
 })
 
+// get summoner profile data
 app.get('/api/profile/:summoner', (req, res) => {
   let param = req.params.summoner;
-  let summoner_data = {}
+  let summonerData = {}
   axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${param}?api_key=${process.env.RIOT_API_KEY}`)
     .then(res => {
-      summoner_data = res.data;
-      let encryptedSummonerId = summoner_data.id;
-      axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encryptedSummonerId}?api_key=${process.env.RIOT_API_KEY}`)
-        .then(res => {
-          console.log(res.data)
-        })
-        .catch(err => {
-          console.log(err);
-        })
+      const data = res.data;
+      summonerData = {
+        summonerId: data.id,
+        accountId: data.accountId,
+        name: data.name,
+        profileIconId: data.profileIconId,
+        summonerLevel: data.summonerLevel,
+      }
     })
     .catch(err => {
       console.log(err);
     })
     .then(() => {
-      let profileIconId	= summoner_data.profileIconId
-      res.send(`http://ddragon.leagueoflegends.com/cdn/11.13.1/img/profileicon/${profileIconId}.png`)
+      res.send(summonerData)
+      //let profileIconId	= summonerData.profileIconId
+      //res.send(`http://ddragon.leagueoflegends.com/cdn/11.13.1/img/profileicon/${profileIconId}.png`)
     });  
+})
+
+app.get('/api/profile/ranked/:summonerId', (req, res) => {
+  let param = req.params.summonerId;
+  let summonerRankedData = {};
+  let encryptedSummonerId = param;
+  axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encryptedSummonerId}?api_key=${process.env.RIOT_API_KEY}`)
+    .then(res => {
+      res.data.forEach(el => (
+        summonerRankedData[el.queueType] = {
+          tier: el.tier,
+          rank: el.rank,
+          LP: el.leaguePoints,
+          wins: el.wins,
+          losses: el.losses,
+          winrate: (el.wins / (el.wins + el.losses) * 100).toFixed(2),
+          totalGames: el.wins + el.losses,
+        }
+      ))
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .then(() => {
+      res.send(summonerRankedData)
+      //let profileIconId	= summonerData.profileIconId
+      //res.send(`http://ddragon.leagueoflegends.com/cdn/11.13.1/img/profileicon/${profileIconId}.png`)
+    });
 })
 
 // All other GET requests not handled before will return our React app
